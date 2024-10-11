07c9cae97afdfa1afa03cebca5eea506e9b14e7c
Nabilabintaakter <nabilabintaakter@gmail.com>
2024-10-11 08:56:36 +0600

    added latest posts section and its functionalities

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // fetch data:
 const loadAllPosts = async (category) => {
-
     const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${category ? `?category=${category}` : ''}`);
     const data = await response.json();
     displayAllPosts(data.posts);
+}
+// load latest posts
+const loadLatestPosts = async () => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
+    const data = await response.json();
+    displayLatestPosts(data);
 }
 
 
@@ -95,7 +100,54 @@ const handleCount=()=>{
 
 }
 
+// display latest posts
+const displayLatestPosts = (postsArray)=>{
+    postsArray.forEach(post=>{
+        const latestPostContainer = document.getElementById('latest-post-container');
+        const div = document.createElement('div');
+        div.innerHTML=`
+        <div class="card lg:w-96 pb-5 bg-base-100      shadow-2xl">
+                    <figure class="lg:px-6 px-4 pt-4 lg:pt-8">
+                        <img src=${post.cover_image} alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="p-5 lg:p-10 space-y-4 
+                       lg:space-y-5">
+                        <p class="opacity-50 text-start">
+                            <i class="fa-solid fa-calendar-days me-2"></i>${post.author?.posted_date || "No Publish Date"}
+                        </p>
+                        <h2 class="card-title text-start ">${post.title || "No Title"}</h2>
+                        <p class="text-start pb-3">
+                        ${post.description || "No description"}
+                        </p>
+                        <div class="card-actions flex gap-5 items-center">
+                            <div class="avatar">
+                                <div
+                                    class="lg:w-12 w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                    <img src=${post.profile_image} />
+                                </div>
+                            </div>
+                            <div>
+                                <h3 class="text-start font-extrabold">${post.author?.name || "Unknown"}</h3>
+                                <p class="text-start opacity-60">${post.author?.designation || "Unknown"}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+        latestPostContainer.append(div);
+    })
+}
 
+// "cover_image": "https://i.ibb.co/VYGSkLz/pexels-jeshootscom-442576.jpg",
+// "profile_image": "https://i.ibb.co/z8zx95w/pexels-davide-de-giovanni-1649675.jpg",
+// "title": "Gaming Enthusiast Expert in Play",
+// "description": "Leading gaming expert with a wealth of knowledge and passion for all things gaming",
+// "author": {
+// "name": "John Doe",
+// "designation": "ROR Developer",
+// "posted_date": "29 January 2024"
+// }
 
 // globally calling function
 loadAllPosts();
+loadLatestPosts();
